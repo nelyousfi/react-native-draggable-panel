3aab25348d7ca3dbdd92f24bd98011cda8c8db4e
Naoufal <n.elyousfi@gmail.com>
2020-06-30 19:47:43 +0200

    Upgrade to version 1.1.0 and use react native modal instead of react-native-modal

--- src/DraggablePanel.tsx
+++ src/DraggablePanel.tsx
@@ -2,224 +2,228 @@ import * as React from 'react';
 import {
   Animated,
   Dimensions,
-  NativeScrollEvent,
-  NativeSyntheticEvent,
+  Modal,
   ScrollView,
   StyleSheet,
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
-import Modal from 'react-native-modal';
+import {ReactNode} from 'react';
 
-const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_HEIGHT: number = Dimensions.get('window').height;
 const DEFAULT_PANEL_HEIGHT = SCREEN_HEIGHT - 100;
 
 type Props = {
-  children: React.ReactNode;
-  visible: boolean;
+  children: ReactNode;
+  visible?: boolean;
   animationDuration: number;
   expandable: boolean;
-  hideOnPressOutside: boolean;
-  overlayBackgroundColor: string;
+  hideOnPressOutside?: boolean;
+  overlayBackgroundColor?: string;
   overlayOpacity: number;
   borderRadius: number;
-  height: number;
+  initialHeight: number;
   onDismiss?: () => void;
 };
 
-export const DraggablePanel = React.forwardRef((props: Props, ref) => {
-  const [animatedValue] = React.useState(new Animated.Value(0));
-  const [popupVisible, togglePopupVisibility] = React.useState(false);
-  const [animating, setAnimating] = React.useState(false);
-  const [height] = React.useState(Math.min(props.height, DEFAULT_PANEL_HEIGHT));
-  const [innerContentHeight, setInnerContentHeight] = React.useState(
-    Math.min(props.height, DEFAULT_PANEL_HEIGHT),
-  );
-  const scrollViewRef = React.useRef<any>();
+export const DraggablePanel = React.forwardRef<Props, any>(
+  (
+    {
+      visible = false,
+      animationDuration = 500,
+      expandable = false,
+      hideOnPressOutside = true,
+      overlayBackgroundColor = 'black',
+      overlayOpacity = 0.8,
+      borderRadius = 0,
+      initialHeight = DEFAULT_PANEL_HEIGHT / 2,
+      onDismiss,
+      children,
+    }: Props,
+    ref: any,
+  ) => {
+    const [animatedValue] = React.useState(new Animated.Value(0));
+    const [popupVisible, togglePopupVisibility] = React.useState(false);
+    const [animating, setAnimating] = React.useState(false);
+    const [height] = React.useState(
+      Math.min(initialHeight, DEFAULT_PANEL_HEIGHT),
+    );
+    const [innerContentHeight, setInnerContentHeight] = React.useState(
+      Math.min(initialHeight, DEFAULT_PANEL_HEIGHT),
+    );
+    const scrollViewRef = React.useRef<any>();
 
-  React.useEffect(() => {
-    if (!animating) {
-      if (props.visible && !popupVisible) {
-        show();
-      } else if (popupVisible) {
-        hide();
+    React.useEffect(() => {
+      if (!animating) {
+        if (visible && !popupVisible) {
+          show();
+        } else if (popupVisible) {
+          hide();
+        }
       }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.visible]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [visible]);
 
-  const show = () => {
-    if (!animating) {
-      animatedValue.setValue(0);
-      setInnerContentHeight(Math.min(props.height, DEFAULT_PANEL_HEIGHT));
-      setAnimating(true);
-      togglePopupVisibility(true);
-      Animated.timing(animatedValue, {
-        toValue: height / DEFAULT_PANEL_HEIGHT,
-        duration: props.animationDuration,
-        useNativeDriver: true,
-      }).start(() => {
-        scrollViewRef.current!.scrollTo({
-          x: 0,
-          y: SCREEN_HEIGHT - (SCREEN_HEIGHT * height) / DEFAULT_PANEL_HEIGHT,
-          animated: false,
+    const show = () => {
+      if (!animating) {
+        animatedValue.setValue(0);
+        setInnerContentHeight(Math.min(initialHeight, DEFAULT_PANEL_HEIGHT));
+        setAnimating(true);
+        togglePopupVisibility(true);
+        Animated.timing(animatedValue, {
+          toValue: height / DEFAULT_PANEL_HEIGHT,
+          duration: animationDuration,
+          useNativeDriver: true,
+        }).start(() => {
+          scrollViewRef.current?.scrollTo({
+            x: 0,
+            y: SCREEN_HEIGHT - (SCREEN_HEIGHT * height) / DEFAULT_PANEL_HEIGHT,
+            animated: false,
+          });
+          setAnimating(false);
         });
-        setAnimating(false);
-      });
-    }
-  };
+      }
+    };
 
-  const hide = () => {
-    if (!animating) {
-      setAnimating(true);
-      Animated.timing(animatedValue, {
-        toValue: 0,
-        duration: props.animationDuration,
-        useNativeDriver: true,
-      }).start(() => {
-        scrollViewRef.current!.scrollTo({
-          x: 0,
-          y: SCREEN_HEIGHT,
-          animated: false,
+    const hide = React.useCallback(() => {
+      if (!animating) {
+        setAnimating(true);
+        Animated.timing(animatedValue, {
+          toValue: 0,
+          duration: animationDuration,
+          useNativeDriver: true,
+        }).start(() => {
+          scrollViewRef.current?.scrollTo({
+            x: 0,
+            y: SCREEN_HEIGHT,
+            animated: false,
+          });
+          togglePopupVisibility(false);
+          setAnimating(false);
+          onDismiss && onDismiss();
         });
-        togglePopupVisibility(false);
-        setAnimating(false);
-        props.onDismiss && props.onDismiss();
-      });
-    }
-  };
-
-  React.useImperativeHandle(ref, () => ({
-    show,
-    hide,
-  }));
-
-  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-    if (!animating) {
-      const {y} = event.nativeEvent.contentOffset;
-      if (
-        !props.expandable &&
-        y < SCREEN_HEIGHT - (SCREEN_HEIGHT * height) / DEFAULT_PANEL_HEIGHT
-      ) {
-        return;
       }
-      animatedValue.setValue(1 - Math.floor(y) / Math.floor(SCREEN_HEIGHT));
-      // >= Fix the android issue, cause for some reason it goes for more than SCREEN_HEIGHT
-      // if the use swipes faster
-      if (Math.floor(y) >= Math.floor(SCREEN_HEIGHT)) {
-        togglePopupVisibility(false);
-        setAnimating(false);
-        props.onDismiss && props.onDismiss();
-      }
-    }
-  };
+    }, [animatedValue, animating, animationDuration, onDismiss]);
 
-  const onScrollBeginDrag = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    if (e.nativeEvent.contentOffset.y !== 0 && props.expandable) {
-      setInnerContentHeight(DEFAULT_PANEL_HEIGHT);
-    }
-  };
+    React.useImperativeHandle(ref, () => ({
+      show,
+      hide,
+    }));
 
-  const onMomentumScrollEnd = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    if (props.expandable) {
-      const {y} = e.nativeEvent.contentOffset;
-      if (y !== 0) {
-        setInnerContentHeight(height);
-      } else {
-        setInnerContentHeight(DEFAULT_PANEL_HEIGHT);
-      }
-    }
-  };
+    const onScroll = React.useCallback(
+      (event: any) => {
+        if (!animating) {
+          const {y} = event.nativeEvent.contentOffset;
+          if (
+            !expandable &&
+            y < SCREEN_HEIGHT - (SCREEN_HEIGHT * height) / DEFAULT_PANEL_HEIGHT
+          ) {
+            return;
+          }
+          animatedValue.setValue(1 - Math.floor(y) / Math.floor(SCREEN_HEIGHT));
+          // >= Fix the android issue, cause for some reason it goes for more than SCREEN_HEIGHT
+          // if the use swipes faster
+          if (Math.floor(y) >= Math.floor(SCREEN_HEIGHT)) {
+            togglePopupVisibility(false);
+            setAnimating(false);
+            onDismiss && onDismiss();
+          }
+        }
+      },
+      [animatedValue, animating, expandable, height, onDismiss],
+    );
 
-  return (
-    <Modal
-      isVisible={popupVisible}
-      backdropOpacity={0}
-      animationInTiming={1}
-      animationOutTiming={1}
-      animationIn={'fadeIn'}
-      animationOut={'fadeOut'}
-      style={styles.modal}>
-      <View style={styles.popupContainer}>
-        <Animated.View
-          style={{
-            ...styles.popupOverlay,
-            backgroundColor: props.overlayBackgroundColor,
-            opacity: animatedValue.interpolate({
-              inputRange: [0, height / DEFAULT_PANEL_HEIGHT],
-              outputRange: [0, props.overlayOpacity],
-              extrapolate: 'clamp',
-            }),
-          }}
-        />
-        <ScrollView
-          ref={scrollViewRef}
-          style={styles.scroll}
-          scrollEventThrottle={16}
-          contentContainerStyle={styles.scrollContainer}
-          onScroll={onScroll}
-          bounces={false}
-          showsVerticalScrollIndicator={false}
-          onScrollBeginDrag={onScrollBeginDrag}
-          onMomentumScrollEnd={onMomentumScrollEnd}
-          decelerationRate={0}
-          snapToOffsets={[
-            0,
-            SCREEN_HEIGHT - (SCREEN_HEIGHT * height) / DEFAULT_PANEL_HEIGHT,
-            SCREEN_HEIGHT,
-          ]}>
-          <TouchableWithoutFeedback
-            style={styles.hideContainer}
-            disabled={!props.hideOnPressOutside || animating}
-            onPress={hide}>
-            <View style={styles.hideContainer} />
-          </TouchableWithoutFeedback>
-        </ScrollView>
-        <Animated.View
-          style={[
-            styles.popupContentContainer,
-            {
-              borderTopLeftRadius: props.borderRadius,
-              borderTopRightRadius: props.borderRadius,
-              transform: [
-                {
-                  translateY: animatedValue.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0, -DEFAULT_PANEL_HEIGHT],
-                  }),
-                },
-              ],
-            },
-          ]}>
-          <View style={styles.indicator} />
-          <View
-            style={[
-              styles.content,
-              {height: props.expandable ? innerContentHeight : height},
+    const onScrollBeginDrag = React.useCallback(
+      (e: any) => {
+        if (e.nativeEvent.contentOffset.y !== 0 && expandable) {
+          setInnerContentHeight(DEFAULT_PANEL_HEIGHT);
+        }
+      },
+      [expandable],
+    );
+
+    const onMomentumScrollEnd = React.useCallback(
+      (e: any) => {
+        if (expandable) {
+          const {y} = e.nativeEvent.contentOffset;
+          if (y !== 0) {
+            setInnerContentHeight(height);
+          } else {
+            setInnerContentHeight(DEFAULT_PANEL_HEIGHT);
+          }
+        }
+      },
+      [height, expandable],
+    );
+
+    return (
+      <Modal visible={popupVisible} transparent animated={false}>
+        <View style={styles.popupContainer}>
+          <Animated.View
+            style={{
+              ...styles.popupOverlay,
+              backgroundColor: overlayBackgroundColor,
+              opacity: animatedValue.interpolate({
+                inputRange: [0, height / DEFAULT_PANEL_HEIGHT],
+                outputRange: [0, overlayOpacity],
+                extrapolate: 'clamp',
+              }),
+            }}
+          />
+          <ScrollView
+            ref={scrollViewRef}
+            style={styles.scroll}
+            scrollEventThrottle={16}
+            contentContainerStyle={styles.scrollContainer}
+            onScroll={onScroll}
+            bounces={false}
+            showsVerticalScrollIndicator={false}
+            onScrollBeginDrag={onScrollBeginDrag}
+            onMomentumScrollEnd={onMomentumScrollEnd}
+            decelerationRate={0}
+            snapToOffsets={[
+              0,
+              SCREEN_HEIGHT - (SCREEN_HEIGHT * height) / DEFAULT_PANEL_HEIGHT,
+              SCREEN_HEIGHT,
             ]}>
-            {props.children}
-          </View>
-        </Animated.View>
-      </View>
-    </Modal>
-  );
-});
-
-DraggablePanel.defaultProps = {
-  visible: false,
-  animationDuration: 500,
-  expandable: false,
-  hideOnPressOutside: true,
-  overlayBackgroundColor: 'black',
-  overlayOpacity: 0.8,
-  borderRadius: 0,
-  height: DEFAULT_PANEL_HEIGHT / 2,
-};
+            <TouchableWithoutFeedback
+              disabled={!hideOnPressOutside || animating}
+              onPress={hide}>
+              <View style={styles.hideContainer} />
+            </TouchableWithoutFeedback>
+          </ScrollView>
+          <Animated.View
+            style={[
+              styles.popupContentContainer,
+              {
+                borderTopLeftRadius: borderRadius,
+                borderTopRightRadius: borderRadius,
+                transform: [
+                  {
+                    translateY: animatedValue.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, -DEFAULT_PANEL_HEIGHT],
+                    }),
+                  },
+                ],
+              },
+            ]}>
+            <View style={styles.indicator} />
+            <View
+              style={[
+                styles.content,
+                {height: expandable ? innerContentHeight : height},
+              ]}>
+              {children}
+            </View>
+          </Animated.View>
+        </View>
+      </Modal>
+    );
+  },
+);
 
 const styles = StyleSheet.create({
-  modal: {
-    margin: 0,
-  },
   popupContainer: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: 'transparent',
